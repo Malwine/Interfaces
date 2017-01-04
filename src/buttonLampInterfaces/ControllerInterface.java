569b945010efececf4616be263a67f71df771c8b
Malwine <malwine@mailbox.org>
2017-01-04 16:09:39 +0100

    delete sendMessage

--- src/buttonLampInterfaces/ControllerInterface.java
+++ src/buttonLampInterfaces/ControllerInterface.java
@@ -2,8 +2,6 @@ package buttonLampInterfaces;
 
 
 public interface ControllerInterface extends java.rmi.Remote {
-	
-	public void sendMessage(String message) throws java.rmi.RemoteException;
 
 	public void update() throws java.rmi.RemoteException;
 
