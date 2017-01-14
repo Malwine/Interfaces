9d38143ff54869ba6a9b3d93cf1455daee3eddf2
Malwine <malwine@mailbox.org>
2017-01-14 17:50:01 +0100

    Add version parameter for development

--- src/buttonLampInterfaces/ControllerInterface.java
+++ src/buttonLampInterfaces/ControllerInterface.java
@@ -1,8 +1,11 @@
 package buttonLampInterfaces;
 
+import java.rmi.RemoteException;
 
 public interface ControllerInterface extends java.rmi.Remote {
 
-	public void update() throws java.rmi.RemoteException;
+	//public void update() throws java.rmi.RemoteException;
+
+	public void update(String version) throws RemoteException;
 
 }
